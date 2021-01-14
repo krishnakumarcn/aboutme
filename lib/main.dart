99a277686e3b45f41bd33e160f9262e117be3649
krishnakumarcn <krishnacnskp@gmail.com>
2021-01-14 11:50:33 +0530

    bg changed

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
         title: Text(widget.title),
       ),
