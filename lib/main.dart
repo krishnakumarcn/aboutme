c90166f52bc6261ddacb22d3d167c61bf2b2d6cf
krishnakumarcn <krishnacnskp@gmail.com>
2021-01-26 13:15:09 +0530

    Init code for new layout dark/light with stacked.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
 import 'package:aboutme/views/home/home_desktop_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:stacked_themes/stacked_themes.dart';
 
-void main() {
+import 'app/locator.dart';
+
+Future main() async {
+  await ThemeManager.initialise();
+  setupLocator();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '@krishnakumarcn',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: GoogleFonts.varela().fontFamily,
+    return ThemeBuilder(
+      defaultThemeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.blue[700],
+        accentColor: Colors.yellow[700],
+        primaryColor: Colors.blue[700],
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.yellow[700],
+        ),
       ),
-      home: HomePage(),
+      lightTheme: ThemeData(
+        brightness: Brightness.light,
+        backgroundColor: Color(0xffff4083),
+        accentColor: Color(0xff009a8c),
+        primaryColor: Color(0xffff4083),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xff009a8c),
+        ),
+      ),
+      builder: (context, regularTheme, darkTheme, themeMode) => MaterialApp(
+        title: '@krishnakumarcn',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: GoogleFonts.varela().fontFamily,
+        ),
+        home: HomePage(),
+      ),
     );
   }
 }
--- lib/views/home/home_desktop_view.dart
+++ lib/views/home/home_desktop_view.dart
@@ -22,56 +22,99 @@ class HomeDesktopView extends StatelessWidget {
     return Scaffold(
         body: ResponsiveBuilder(
             builder: (context, sizingInformation) => Center(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              width:
-                                  (sizingInformation.screenSize.width * 0.36) +
-                                      48,
-                              color: Color(0xff36454F),
-                            ),
-                            Expanded(
-                              child: Container(
-                                color: Color(0xff23636d),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
+                    child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
                         height: sizingInformation.screenSize.height * 0.75,
-                        // width: sizingInformation.screenSize.width * 1,
-                        padding: EdgeInsets.symmetric(horizontal: 48),
-                        child: Card(
-                          elevation: 10.0,
-                          margin: EdgeInsets.zero,
-                          child: Container(
-                            child: Row(
-                              children: [
-                                Center(
-                                  child: Image.asset(
-                                    "assets/kk.jpg",
-                                    height:
-                                        sizingInformation.screenSize.height *
-                                            0.75,
-                                    width: sizingInformation.screenSize.width *
-                                        0.36,
-                                    fit: BoxFit.cover,
+                        // color: Color(0xffcfcfcf),
+                        child: Container(
+                          margin: EdgeInsets.only(left: 48),
+                          child: Card(
+                            elevation: 10.0,
+                            margin: EdgeInsets.zero,
+                            color: Colors.transparent,
+                            child: SizedBox(
+                              child: Center(
+                                child: Align(
+                                  alignment: Alignment.centerRight,
+                                  child: AspectRatio(
+                                    aspectRatio: 1,
+                                    child: Image.asset(
+                                      "assets/kk.jpg",
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                                Expanded(child: getDetails())
-                              ],
+                              ),
                             ),
                           ),
-                        ),
+                        )),
+                    Expanded(
+                      child: Container(
+                        height: sizingInformation.screenSize.height * 0.75,
+                        margin: EdgeInsets.only(right: 48),
+                        color: Color(0xff23636d),
+                        child: Card(
+                            elevation: 10.0,
+                            margin: EdgeInsets.zero,
+                            color: Colors.transparent,
+                            child: getDetails()),
                       ),
-                    ],
-                  ),
-                )));
+                    ),
+                  ],
+                )
+
+                    // Stack(
+                    //   alignment: Alignment.center,
+                    //   children: [
+                    //     Container(
+                    //       child: Row(
+                    //         children: [
+                    //           Container(
+                    //             width:
+                    //                 (sizingInformation.screenSize.width * 0.36) +
+                    //                     52,
+                    //             color: Color(0xffcfcfcf),
+                    //           ),
+                    //           Expanded(
+                    //             child: Container(
+                    //               color: Color(0xff23636d),
+                    //             ),
+                    //           ),
+                    //         ],
+                    //       ),
+                    //     ),
+                    //     Container(
+                    //       height: sizingInformation.screenSize.height * 0.75,
+                    //       // width: sizingInformation.screenSize.width * 1,
+                    //       padding: EdgeInsets.symmetric(horizontal: 48),
+                    //       child: Card(
+                    //         elevation: 10.0,
+                    //         color: Colors.transparent,
+                    //         child: Container(
+                    //           child: Row(
+                    //             children: [
+                    //               Center(
+                    //                 child: Image.asset(
+                    //                   "assets/kk.jpg",
+                    //                   height:
+                    //                       sizingInformation.screenSize.height *
+                    //                           0.75,
+                    //                   width: sizingInformation.screenSize.width *
+                    //                       0.36,
+                    //                   fit: BoxFit.cover,
+                    //                 ),
+                    //               ),
+                    //               Expanded(child: getDetails())
+                    //             ],
+                    //           ),
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ],
+                    // ),
+                    )));
   }
 
   getExternalLinkButton({String title, String link, String tooltip}) {
@@ -139,7 +182,7 @@ class HomeDesktopView extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 120.0),
             child: Text(
-              "Engineer\t Movie Enthusiast\t Technology devotee\t Flutter fanatic\t Reader",
+              "Engineer\t Reader\t Movie Enthusiast\t Technology devotee\t Flutter fanatic\t ",
               textAlign: TextAlign.center,
               style: TextStyle(
                   fontSize: 20,
