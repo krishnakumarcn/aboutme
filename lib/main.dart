21119539ba8c2b0f9158a1ab1c6d70054ba60f3a
krishnakumarcn <krishnacnskp@gmail.com>
2021-03-19 12:10:02 +0530

    Configure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:krishnakumarcn/app/configure_nonweb.dart';
+import 'app/configure_nonweb.dart'
+    if (dart.library.html) 'app/configure_web.dart';
 
 void main() {
+  configureApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle());
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
