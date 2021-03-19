18c111edf57a1c90bf273172b5dae2aa69a9aaa4
krishnakumarcn <krishnacnskp@gmail.com>
2021-03-19 12:15:52 +0530

    Brand new

--- android/app/src/main/kotlin/cn/krishnakumar/krishnakumarcn/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package cn.krishnakumar.krishnakumarcn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ krishnakumar_cn/android/app/src/main/kotlin/cn/krishnakumar/krishnakumar_cn/MainActivity.kt
@@ -0,0 +1,6 @@
+package cn.krishnakumar.krishnakumar_cn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
